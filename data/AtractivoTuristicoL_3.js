470b70acf2b7f29b03e7b42f069255791f05115e
quevin69 <kevinmarcelo88@gmail.com>
2025-07-06 16:59:29 -0400

    Add files via upload

---
+++ data/AtractivoTuristicoL_3.js
@@ -0,0 +1,1 @@
+var json_AtractivoTuristicoL_3 = {"type":"FeatureCollection","name":"AtractivoTuristicoL_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
